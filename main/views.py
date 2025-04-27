12d4d54646f17c1d7e154fcb3bd2a19c9dd2336a
Suvam Naskar <suvamnaskar780@gmail.com>
2025-04-27 20:11:10 +0530

    DEPLOY: EC2 Deployment v4.1

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Docker Django World!")
+    return HttpResponse("Hello, Docker Django World!<br>Docker is a life saver :)")
